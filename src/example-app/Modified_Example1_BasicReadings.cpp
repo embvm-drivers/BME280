05c8fcf7a712866aa06e0e8447f3cbce8488dd33
Phillip Johnston <phillip@embeddedartistry.com>
2022-02-22 11:11:57 -0800

    Refactoring compiles

--- src/example-app/Modified_Example1_BasicReadings.cpp
+++ src/example-app/Modified_Example1_BasicReadings.cpp
@@ -3,13 +3,15 @@
   By: Nathan Seidle
   SparkFun Electronics
   Date: March 9th, 2018
-  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+  License: This code is public domain but you buy me a beer if you use this and we meet someday
+  (Beerware license).
 
   Feel like supporting our work? Buy a board from SparkFun!
   https://www.sparkfun.com/products/14348 - Qwiic Combo Board
   https://www.sparkfun.com/products/13676 - BME280 Breakout Board
-  
-  This example shows how to read humidity, pressure, and current temperature from the BME280 over I2C.
+
+  This example shows how to read humidity, pressure, and current temperature from the BME280 over
+  I2C.
 
   Hardware connections:
   BME280 -> Arduino
@@ -19,48 +21,85 @@
   SCL -> A5
 */
 
+#include "SparkFunBME280.h"
 #include <Arduino.h>
 #include <Wire.h>
-#include "SparkFunBME280.h"
+#include <assert.h>
 
-BME280 mySensor;
+uint8_t BME280_I2C_Addr_7bit = 0x76;
+void BME280ArduinoI2CWrite(uint8_t register_addr, uint8_t data, void* private_data);
+void BME280ArduinoI2CRead(uint8_t register_addr, uint8_t* data_out, size_t length,
+						  void* private_data);
+
+BME280 mySensor(BME280ArduinoI2CWrite, BME280ArduinoI2CRead, I2C_MODE,
+				static_cast<void*>(&BME280_I2C_Addr_7bit));
+
+void BME280ArduinoI2CWrite(uint8_t register_addr, uint8_t data, void* private_data)
+{
+	assert(private_data);
+
+	uint8_t address = *static_cast<uint8_t*>(private_data);
+
+	Wire.beginTransmission(address);
+	Wire.write(register_addr);
+	Wire.write(data);
+	Wire.endTransmission();
+}
+
+void BME280ArduinoI2CRead(uint8_t register_addr, uint8_t* data_out, size_t length,
+						  void* private_data)
+{
+	assert(data_out && private_data);
+
+	uint8_t address = *static_cast<uint8_t*>(private_data);
+	uint8_t i = 0;
+
+	Wire.beginTransmission(address);
+	Wire.write(register_addr);
+	Wire.endTransmission();
+	Wire.requestFrom(address, length);
+	// Loop is to handle the case where device may send less than requested
+	while(Wire.available() and i < length)
+	{
+		data_out[i++] = Wire.read();
+	}
+}
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println("Reading basic values from BME280");
+	Serial.begin(115200);
+	Serial.println("Reading basic values from BME280");
 
-  Wire.begin();
+	Wire.begin();
 
-  if (mySensor.beginI2C() == false) //Begin communication over I2C
-  {
-    Serial.println("The sensor did not respond. Please check wiring.");
-    while(1); //Freeze
-  }
+	if(mySensor.begin() == false)
+	{
+		Serial.println("The sensor did not respond. Please check wiring.");
+		while(1)
+			; // Freeze
+	}
 }
 
 void loop()
 {
-  Serial.print("Humidity: ");
-  Serial.print(mySensor.readFloatHumidity(), 0);
+	Serial.print("Humidity: ");
+	Serial.print(mySensor.readFloatHumidity(), 0);
 
-  Serial.print(" Pressure: ");
-  Serial.print(mySensor.readFloatPressure(), 0);
+	Serial.print(" Pressure: ");
+	Serial.print(mySensor.readFloatPressure(), 0);
 
-  Serial.print(" Alt: ");
-  //Serial.print(mySensor.readFloatAltitudeMeters(), 1);
-  Serial.print(mySensor.readFloatAltitudeFeet(), 1);
+	Serial.print(" Alt: ");
+	// Serial.print(mySensor.readFloatAltitudeMeters(), 1);
+	Serial.print(mySensor.readFloatAltitudeFeet(), 1);
 
-  Serial.print(" Temp: ");
-  //Serial.print(mySensor.readTempC(), 2);
-  Serial.print(mySensor.readTempF(), 2);
+	Serial.print(" Temp: ");
+	// Serial.print(mySensor.readTempC(), 2);
+	Serial.print(mySensor.readTempF(), 2);
 
-  Serial.println();
+	Serial.println();
 
-  delay(50);
+	delay(50);
 }
-
-
 
 #if 0
 // Initialization of SPI
